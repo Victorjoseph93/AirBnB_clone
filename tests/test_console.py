5046a6b3db4fced1f04cf6d0b17e360c4422788c
Victorjoseph93 <victorjoseph001@gmail.com>
2024-11-17 12:45:26 -0800

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for console.py.
-Unittest classes:
-    TestHBNBCommand_prompting
-    TestHBNBCommand_help
-    TestHBNBCommand_exit
-    TestHBNBCommand_create
-    TestHBNBCommand_show
-    TestHBNBCommand_all
-    TestHBNBCommand_destroy
-    TestHBNBCommand_update
-"""
+"""Defines unittests for console.py."""
 import os
 import sys
 import unittest
@@ -42,11 +32,13 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_create(self):
-        h = ("Usage: create <class>\n        "
-             "Create a new class instance and print its id.")
+        h = (
+            "\n        Usage: create <class>\n"
+            "\n        Create a new instance and print its `id`.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help create"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help_EOF(self):
         h = "EOF signal to exit the program."
@@ -55,45 +47,59 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_show(self):
-        h = ("Usage: show <class> <id> or <class>.show(<id>)\n        "
-             "Display the string representation of a class instance of"
-             " a given id.")
+        h = (
+            "\n        Usage: show <class> <id> or <class>.show(<id>)\n\n"
+            "        Display the string representation of an instance of"
+            " a given id.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help show"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help_destroy(self):
-        h = ("Usage: destroy <class> <id> or <class>.destroy(<id>)\n        "
-             "Delete a class instance of a given id.")
+        h = (
+            "\n        Usage: destroy <class> <id> or <class>.distroy(<id>)\n"
+            "\n        Delete the instance of the given id.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help destroy"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help_all(self):
-        h = ("Usage: all or all <class> or <class>.all()\n        "
-             "Display string representations of all instances of a given class"
-             ".\n        If no class is specified, displays all instantiated "
-             "objects.")
+        h = (
+            "\n        Usage: all or all <class> or <class>.all()\n\n"
+            "        Display string representations of all instances of "
+            "a given class.\n"
+            "        If no class is specified, displays all instantiated"
+            " objects.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help all"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help_count(self):
-        h = ("Usage: count <class> or <class>.count()\n        "
-             "Retrieve the number of instances of a given class.")
+        h = (
+            "\n        Usage: count <class> or <class>.count()\n\n"
+            "        Retrive the number of instances"
+            " of a given class.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help count"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help_update(self):
-        h = ("Usage: update <class> <id> <attribute_name> <attribute_value> or"
-             "\n       <class>.update(<id>, <attribute_name>, <attribute_value"
-             ">) or\n       <class>.update(<id>, <dictionary>)\n        "
-             "Update a class instance of a given id by adding or updating\n   "
-             "     a given attribute key/value pair or dictionary.")
+        h = (
+            "\n        Usage: update <class name> <id> <attribute name>"
+            " \"<attribute value>\" or\n"
+            "        <class>.update(<id>, <attribute name>,"
+            " \"<attribute value>\") or\n        "
+            "<class>.update(<id>, <dictionary>).\n\n"
+            "        Update the instance of a given id by adding or\n"
+            "        updating a given attribute.\n        \n"
+        )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help update"))
-            self.assertEqual(h, output.getvalue().strip())
+            self.assertEqual(h, output.getvalue())
 
     def test_help(self):
         h = ("Documented commands (type help <topic>):\n"
@@ -151,11 +157,11 @@ class TestHBNBCommand_create(unittest.TestCase):
             self.assertEqual(correct, output.getvalue().strip())
 
     def test_create_invalid_syntax(self):
-        correct = "*** Unknown syntax: MyModel.create()"
+        correct = "** Unknown syntax: MyModel.create()"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("MyModel.create()"))
             self.assertEqual(correct, output.getvalue().strip())
-        correct = "*** Unknown syntax: BaseModel.create()"
+        correct = "** Unknown syntax: BaseModel.create()"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("BaseModel.create()"))
             self.assertEqual(correct, output.getvalue().strip())
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/amenity.py.
-Unittest classes:
-    TestAmenity_instantiation
-    TestAmenity_save
-    TestAmenity_to_dict
-"""
+"""Defines unittests for models/amenity.py."""
 import os
 import models
 import unittest
