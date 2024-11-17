dfb3883d45a7ca35ae468c8bf2995e540eeab6f6
Victorjoseph93 <victorjoseph001@gmail.com>
2024-11-17 11:51:44 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""This is the module that contains the structure of the User class
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+    """This is the class that contains the attributes and methods of the
+       User class
     """
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """This method instantiates a new User object which is an instance
+           of BaseModel
+        """
+        super().__init__(**kwargs)
